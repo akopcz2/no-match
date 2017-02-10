8c2765114042c64b20176e905cedc3726c861cef
Adrian Kopczewski <adrian.kopczewski@designory.com>
2017-02-10 12:08:41 -0600

    Adding app root path to dependency

--- index.js
+++ index.js
@@ -6,46 +6,51 @@ var imagesFolder = appRoot + '/build/static/images/';
 var imageDirectory = '/build/static/images/';
 var prefix = 'Your custom prefix'
 
-//Get prefix from deployment file
-var deploymentFile = JSON.parse(fs.readFileSync('./deployment.json', 'utf8'));
-if(deploymentFile.prefix){
-    prefix = JSON.stringify(deploymentFile.prefix).replace(/\"/g, "").toLowerCase();
-}
-else{
-    console.log('You are missing a image prefix in your deployment.json');
-}
+module.exports = {
+    init:function(){
 
-var deleteTheseImages = [];
+        //Get prefix from deployment file
+        var deploymentFile = JSON.parse(fs.readFileSync('./deployment.json', 'utf8'));
+        if(deploymentFile.prefix){
+            prefix = JSON.stringify(deploymentFile.prefix).replace(/\"/g, "").toLowerCase();
+        }
+        else{
+            console.log('You are missing a image prefix in your deployment.json');
+        }
 
-//get images from build
-function getImages(callback){
-    fs.realpath(__dirname, function (err, path) {
-        if (err) throw err;
-        var fileLocation = path;
-        //finds images in directory
-        fs.readdir(imagesFolder, (err, files) => {
-            files.forEach(file => {
-                if(file.indexOf(prefix) == -1){
-                var fullPath = fileLocation + imageDirectory  + file;
-                fullPath = fullPath.split('node_modules/no-match/').join('');
-                deleteTheseImages.push(fullPath);
-                }
+        var deleteTheseImages = [];
+
+        //get images from build
+        function getImages(callback){
+            fs.realpath(__dirname, function (err, path) {
+                if (err) throw err;
+                var fileLocation = path;
+                //finds images in directory
+                fs.readdir(imagesFolder, (err, files) => {
+                    files.forEach(file => {
+                        if(file.indexOf(prefix) == -1){
+                        var fullPath = fileLocation + imageDirectory  + file;
+                        fullPath = fullPath.split('node_modules/no-match/').join('');
+                        deleteTheseImages.push(fullPath);
+                        }
+                    });
+                    callback(deleteTheseImages);
+                    return deleteTheseImages;
+                });
             });
-            callback(deleteTheseImages);
-            return deleteTheseImages;
+        }
+        getImages(function(images){
+            deletePath();
         });
-    });
-}
-getImages(function(images){
-    deletePath();
-});
 
-function deletePath(){
-    for(var i = 0; i < deleteTheseImages.length; i ++){
-        console.log(deleteTheseImages[i]);
-        fs.unlink(deleteTheseImages[i] ,function(err){
-            if(err) throw err;
-        });
+        function deletePath(){
+            for(var i = 0; i < deleteTheseImages.length; i ++){
+                console.log(deleteTheseImages[i]);
+                fs.unlink(deleteTheseImages[i] ,function(err){
+                    if(err) throw err;
+                });
+            }
+        }
     }
 }
 
